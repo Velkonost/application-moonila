709842071a439a1992e65a6ab13f1c0af0c03c4d
Artem Klimenko <17786225+Velkonost@users.noreply.github.com>
2024-03-11 12:12:20 +0600

    feed

--- features/feed/presentation/src/main/kotlin/features/feed/presentation/screen/FeedScreen.kt
+++ features/feed/presentation/src/main/kotlin/features/feed/presentation/screen/FeedScreen.kt
@@ -1,7 +1,10 @@
 package features.feed.presentation.screen
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import features.feed.presentation.FeedViewModel
 
 @Composable
@@ -9,4 +12,11 @@ fun FeedScreen(
     modifier: Modifier = Modifier,
     viewModel: FeedViewModel
 ) {
+
+    val state by viewModel.viewState.collectAsStateWithLifecycle()
+
+    Column {
+
+    }
+
 }

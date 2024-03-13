a13dcdbdab2d26022fa2a6c6672cfcb1108f9175
Artem Klimenko <17786225+Velkonost@users.noreply.github.com>
2024-03-13 12:49:59 +0600

    feed

--- features/feed/presentation/src/main/kotlin/features/feed/presentation/model/MoonTipsItem.kt
+++ features/feed/presentation/src/main/kotlin/features/feed/presentation/model/MoonTipsItem.kt
@@ -1,10 +1,42 @@
 package features.feed.presentation.model
 
+import com.moonila.features.feed.presentation.R
+
 enum class MoonTipsItem(
     val titleResId: Int,
     val iconId: Int,
     val inactiveIconId: Int
-)
+) {
+    Health(
+        titleResId = R.string.moon_tips_health,
+        iconId = R.drawable.ic_tips_health,
+        inactiveIconId = R.drawable.ic_tips_health_inactive
+    ),
+
+    Business(
+        titleResId = R.string.moon_tips_business,
+        iconId = R.drawable.ic_tips_business,
+        inactiveIconId = R.drawable.ic_tips_business_inactive
+    ),
+
+    Beauty(
+        titleResId = R.string.moon_tips_beauty,
+        iconId = R.drawable.ic_tips_beauty,
+        inactiveIconId = R.drawable.ic_tips_beauty_inactive
+    ),
+
+    Nutrition(
+        titleResId = R.string.moon_tips_nutrition,
+        iconId = R.drawable.ic_tips_nutrition,
+        inactiveIconId = R.drawable.ic_tips_nutrition_inactive
+    ),
+
+    Relations(
+        titleResId = R.string.moon_tips_relations,
+        iconId = R.drawable.ic_tips_relations,
+        inactiveIconId = R.drawable.ic_tips_relations_inactive
+    ),
+}
 
 data class MoonTipContent(
     val type: MoonTipsItem,

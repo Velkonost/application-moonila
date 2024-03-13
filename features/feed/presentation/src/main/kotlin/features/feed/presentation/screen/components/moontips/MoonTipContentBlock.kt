6d6a98dae51b486f96c22ae12fb8637582a7bd40
Artem Klimenko <17786225+Velkonost@users.noreply.github.com>
2024-03-13 13:09:48 +0600

    feed

--- features/feed/presentation/src/main/kotlin/features/feed/presentation/screen/components/moontips/MoonTipContentBlock.kt
+++ features/feed/presentation/src/main/kotlin/features/feed/presentation/screen/components/moontips/MoonTipContentBlock.kt
@@ -1,12 +1,14 @@
 package features.feed.presentation.screen.components.moontips
 
+import android.annotation.SuppressLint
+import androidx.compose.animation.AnimatedContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
@@ -16,6 +18,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
@@ -24,10 +27,11 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.moonila.features.feed.presentation.R
 import core.compose.theme.PoppinsFontFamily
 import features.feed.presentation.model.MoonTipContent
-import com.moonila.features.feed.presentation.R
 
+@SuppressLint("UnusedContentLambdaTargetStateParameter")
 @Composable
 fun MoonTipContentBlock(
     modifier: Modifier = Modifier,
@@ -37,49 +41,60 @@ fun MoonTipContentBlock(
     Box(
         modifier = modifier
             .padding(top = 16.dp, start = 16.dp, end = 16.dp)
+            .height(IntrinsicSize.Max)
             .background(
                 color = colorResource(id = R.color.tip_selected_bg),
                 shape = RoundedCornerShape(16.dp)
             )
-            .padding(20.dp)
     ) {
         Column {
             Spacer(modifier.weight(1f))
             Image(
+                modifier = modifier.alpha(0.2f),
                 painter = painterResource(id = R.drawable.ic_tip_bg),
                 contentDescription = null
             )
         }
 
-        Column(modifier = modifier.fillMaxWidth()) {
+        Column(
+            modifier = modifier
+                .fillMaxWidth()
+                .padding(20.dp)
+        ) {
             Row(verticalAlignment = Alignment.CenterVertically) {
-                Text(
-                    text = stringResource(id = item.type.titleResId),
-                    fontFamily = PoppinsFontFamily,
-                    fontWeight = FontWeight.SemiBold,
-                    color = colorResource(id = com.moonila.core.compose.R.color.light_color),
-                    fontSize = 20.sp,
-                )
+                AnimatedContent(targetState = item.type.titleResId, label = "") {
+                    Text(
+                        text = stringResource(id = it),
+                        fontFamily = PoppinsFontFamily,
+                        fontWeight = FontWeight.SemiBold,
+                        color = colorResource(id = com.moonila.core.compose.R.color.light_color),
+                        fontSize = 20.sp,
+                    )
+                }
                 Spacer(modifier.weight(1f))
 
-                Image(
-                    modifier = modifier.size(34.dp),
-                    painter = painterResource(id = item.type.iconId),
-                    contentDescription = null
+                AnimatedContent(targetState = item.type.iconId, label = "") {
+                    Image(
+                        modifier = modifier.size(34.dp),
+                        painter = painterResource(id = it),
+                        contentDescription = null
+                    )
+                }
+            }
+
+            AnimatedContent(targetState = item.text, label = "") {
+                Text(
+                    modifier = modifier.padding(top = 20.dp),
+                    text = it,
+                    fontFamily = PoppinsFontFamily,
+                    fontWeight = FontWeight.Normal,
+                    color = colorResource(id = R.color.tip_text_color),
+                    fontSize = 14.sp,
+                    textAlign = TextAlign.Start
                 )
             }
 
-            Text(
-                modifier = modifier.padding(top = 20.dp),
-                text = item.text,
-                fontFamily = PoppinsFontFamily,
-                fontWeight = FontWeight.Normal,
-                color = colorResource(id = R.color.tip_text_color),
-                fontSize = 14.sp,
-                textAlign = TextAlign.Start
-            )
-
-            Row {
+            Row(modifier = modifier.padding(top = 20.dp)) {
                 Spacer(modifier.weight(1f))
 
                 Image(
